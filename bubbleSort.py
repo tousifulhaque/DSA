39b73fdd3d93e9c3b438446f5ca58f025cb536c8
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2021-11-12 17:38:51 +0600

    Selection Sort and Bubble Sort

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+A = [1024, 352, 332, 351,33, 2, 1, 39] # n space complexity
+
+for i in range(len(A)):
+
+    min_idx = i
+    for j in range(i+1, len(A)):
+        if A[min_idx] < A[j]:
+            min_idx = j
+
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+print('Sorted Array')
+for i in range(len(A)):
+    print(A[i])
