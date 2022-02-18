851fb03fd1f0409e90178cec01ac795cd4674501
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-02-18 20:53:10 +0600

    Heap Insertion Complete

--- heap.py
+++ heap.py
@@ -11,30 +11,29 @@ class Heap:
         parent_index  = index #2
         while parent_index > 0 :
             if parent_index % 2  == 1:
-                parent_index = parent_index // 2
+                parent_index = parent_index // 2 #1
             
             else:
-                parent_index = (parent_index // 2) -1
-                
-            if self.heap[index] > self.heap[parent_index]:
-                tmp = self.heap[parent_index]
-                self.heap[parent_index] = self.heap[index]
+                parent_index = (parent_index // 2) - 1 #0
+
+            if self.heap[index] > self.heap[parent_index]: #13 #10
+                tmp = self.heap[parent_index] #0
+                self.heap[parent_index] = self.heap[index] #
                 self.heap[index] = tmp
-                index = parent_index
+                index = parent_index # 1
         
     
     def insert_element(self, element):
         self.heap.append(element)
-        print(self.heap)
         self.size += 1
         self.heapify_up(self.size - 1)
 
 
 if __name__ == "__main__":
-    arr = [32, 10 ,2, 13]
+    arr = [32, 10 ,64, 13, 25, 35, 20]
     heap = Heap()
     for i in arr:
         heap.insert_element(i)
-    
+    print(heap)
 
 
