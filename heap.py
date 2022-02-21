23cc15b729a2f30412b1898d541642bbba71af84
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-02-21 15:14:02 +0600

    Heapify Int

--- heap.py
+++ heap.py
@@ -27,6 +27,7 @@ class Heap:
         self.heap.append(element)
         self.size += 1
         self.heapify_up(self.size - 1)
+
     def heapify_down(self):
         index = 0
         while index < self.size:
@@ -49,16 +50,48 @@ class Heap:
             else:
                 break
 
+    def heapify(self,array):
+    
+        #Check if heap
+        def swap_elements(child, parent):#5, #2
+            tmp = self.heap[parent]
+            self.heap[parent] = self.heap[child]
+            self.heap[child] = tmp
+
+        self.size = len(array) #7 
+        self.heap = array
+        parent_index = self.size - 1 #6
+         # [32, 10 ,64, 13, 25, 65]
+        while parent_index >= 0:  #parent_index = 2
+            # Check if the child element exists
+            left_child_index = (parent_index * 2) + 1 #5
+            right_child_index = (parent_index * 2) + 2 #6
+
+            if right_child_index < self.size:
+                if self.heap[right_child_index] > self.heap[left_child_index]: #65 #30
+                    left_child_index = right_child_index 
+                if self.heap[left_child_index] > self.heap[parent_index]: #65 #64
+                    swap_elements(left_child_index, parent_index)
+                
+            elif left_child_index < self.size:
+                if self.heap[left_child_index] > self.heap[parent_index]: 
+                    swap_elements(left_child_index, parent_index)
+
+            else:
+                pass
+
+            parent_index -= 1 #1
+
+
+
+
     def delete_heap_element(self):
         self.heap[0] = self.heap[self.size - 1]
         self.size -= 1
         self.heapify_down()
 if __name__ == "__main__":
-    arr = [32, 10 ,64, 13, 25, 35, 20]
+    arr = [32, 10 ,64, 13, 25, 65, 30]
     heap = Heap()
-    for i in arr:
-        heap.insert_element(i)
+    heap.heapify(arr)
     print(heap)
-    heap.delete_heap_element()
-    print(heap)
-
+    
