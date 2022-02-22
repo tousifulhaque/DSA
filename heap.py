f88941b52d9217c8b794901f8b3bfb228791f1cf
tousifulhaque <tousifulhaque@iut-dhaka.edu>
2022-02-22 16:41:45 +0600

    Heapifying Array with Recursion

--- heap.py
+++ heap.py
@@ -1,4 +1,7 @@
 ## heap
+from heapq import heapify
+
+
 class Heap:
     def __init__(self):
         self.heap = []
@@ -30,7 +33,9 @@ class Heap:
 
     def heapify_down(self):
         index = 0
+        
         while index < self.size:
+            print(f'From while loop {index}')
             if index * 2 + 1 < self.size:
                 # Getting the index of left child and right child
                 left_child_index = index * 2 + 1
@@ -50,39 +55,35 @@ class Heap:
             else:
                 break
 
-    def heapify(self,array):
-    
-        #Check if heap
-        def swap_elements(child, parent):#5, #2
-            tmp = self.heap[parent]
-            self.heap[parent] = self.heap[child]
-            self.heap[child] = tmp
+    def heap_from_array(self, arr):
+        self.heap = arr
+        self.size = len(arr)
+        for i in range(((self.size -1) // 2), -1, -1):
+            self.heapify(i) #3
 
-        self.size = len(array) #7 
-        self.heap = array
-        parent_index = self.size - 1 #6
+    def heapify(self, i):
+        #Check if heap
+       
+        parent_index = i #3
          # [32, 10 ,64, 13, 25, 65]
-        while parent_index >= 0:  #parent_index = 2
+        while parent_index >= 0 :  #parent_index = 3
             # Check if the child element exists
             left_child_index = (parent_index * 2) + 1 #5
             right_child_index = (parent_index * 2) + 2 #6
-
-            if right_child_index < self.size:
-                if self.heap[right_child_index] > self.heap[left_child_index]: #65 #30
-                    left_child_index = right_child_index 
-                if self.heap[left_child_index] > self.heap[parent_index]: #65 #64
-                    swap_elements(left_child_index, parent_index)
+            if (right_child_index < self.size) and (self.heap[right_child_index] > self.heap[parent_index]): #65 #64
+                parent_index = right_child_index
                 
-            elif left_child_index < self.size:
-                if self.heap[left_child_index] > self.heap[parent_index]: 
-                    swap_elements(left_child_index, parent_index)
 
+            if (left_child_index < self.size) and (self.heap[left_child_index] > self.heap[parent_index]): 
+                parent_index = left_child_index
+                
+            
+            if parent_index != i:
+                self.heap[i], self.heap[parent_index] = self.heap[parent_index], self.heap[i]
+                i = parent_index
+                self.heapify(i)
             else:
-                pass
-
-            parent_index -= 1 #1
-
-
+                break
 
 
     def delete_heap_element(self):
@@ -92,6 +93,5 @@ class Heap:
 if __name__ == "__main__":
     arr = [32, 10 ,64, 13, 25, 65, 30]
     heap = Heap()
-    heap.heapify(arr)
+    heap.heap_from_array(arr)
     print(heap)
-    
